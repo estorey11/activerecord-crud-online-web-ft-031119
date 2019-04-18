f9b303a989fb08fbeb83d56a41baf45b5c5f61ca
Eric Storey <estorey@gmail.com>
2019-04-18 18:43:48 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -59,7 +59,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find
+  Movie.find()
 end
 
 def can_find_using_where_clause_and_be_sorted
