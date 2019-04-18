d27605c204aefb0071ddb526e4647b5625132b3b
Eric Storey <estorey@gmail.com>
2019-04-18 16:27:51 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,11 +20,9 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new
-  attributes.each{|key, value|
-    movie.send("#{key}=", value)
-  }
+  movie = Movie.new(attributes)
   movie.save
+  movie
 end
 
 def can_be_created_in_a_block(args = __)
